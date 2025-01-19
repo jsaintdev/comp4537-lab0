1774a4cabf6f43b50f6cea38dc868a5f05a6acf2
Justin Saint <justinsaintdev@gmail.com>
2025-01-18 16:40:10 -0800

    Created structure for main page, CSS style, and hardcoded strings into user.js

---
+++ COMP4537/labs/1/lang/messages/en/user.js
@@ -0,0 +1,14 @@
+const messages = {
+    "indexTitle": "Simple Note Taker",
+    "writeButton": "Write Notes",
+    "readButton": "Read Notes",
+    "writerTitle": "Write Your Notes",
+    "readerTitle": "Read Your Notes",
+    "storedTime": "Last saved at: ",
+    "retrievedTime": "Last retrieved at: ",
+    "addButton": "Add",
+    "removeButton": "Remove",
+    "backButton": "Back",
+};
+
+export default messages;
