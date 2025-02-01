c95c0f8f84eb9ce087be4b56bf1c852acb7a1b81
Justin Saint <justinsaintdev@gmail.com>
2025-02-01 11:58:17 -0800

    Creating a testing module for Lab 3 Part A

---
+++ COMP4537/labs/3/testing/app.js
@@ -0,0 +1,10 @@
+const math = require('./modules/math');
+
+let a = 10;
+let b = 5;
+
+console.log(`Variables: ${a} and ${b}`);
+
+console.log(`Hello Justin Saint. The sum of the variables is ${math.add(a,b)}`);
+
+console.log(`Difference of ${a} minus ${b} = ${math.subtract(a,b)} `);
