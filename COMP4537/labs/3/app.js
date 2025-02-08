33d354096a960ea076a31afbf8ba4570ea5c20b6
Justin Saint <justinsaintdev@gmail.com>
2025-02-07 19:04:12 -0800

    Starting Lab 4, created necessary files

--- COMP4537/labs/3/app.js
+++ COMP4537/labs/3/app.js
@@ -1,10 +1,13 @@
-const math = require('./modules/math');
-
-let a = 10;
-let b = 5;
-
-console.log(`Variables: ${a} and ${b}`);
-
-console.log(`Hello Justin Saint. The sum of the variables is ${math.add(a,b)}`);
-
-console.log(`Difference of ${a} minus ${b} = ${math.subtract(a,b)} `);
+function myFunc() {
+    let XMLHttpRequest = require();
+    const xhttp = new XMLHttpRequest();
+    xhttp.open(
+      "GET",
+      "https://exo-engine.com/COMP4537/labs/3/getDate/?name=helly", true);
+      xhttp.send();
+      xhttp.onreadystatechange = function () {
+        console.log("helllo");
+      }
+  }
+  
+  myFunc();
