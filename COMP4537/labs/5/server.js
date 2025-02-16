ea1d80c82ade4619963791ecda82b8d83cb0e72a
Justin Saint <justinsaintdev@gmail.com>
2025-02-15 18:57:13 -0800

    Updated server

--- COMP4537/labs/5/server.js
+++ COMP4537/labs/5/server.js
@@ -6,24 +6,33 @@ const PORT = 3000;
 const BASE_DIR = path.join(__dirname, "COMP4537/labs/5");
 
 const server = http.createServer((req, res) => {
-    let filePath = path.join(BASE_DIR, req.url);
-    if (req.url === "/" || req.url === "/COMP4537/labs/5/") {
+    let reqPath = req.url.split("?")[0]; // Remove query params
+    let filePath = path.join(BASE_DIR, reqPath);
+
+    console.log(`Requested Path: ${reqPath}`);
+
+    // Ensure directory requests serve index.html
+    if (reqPath === "/" || reqPath === "/COMP4537/labs/5/") {
         filePath = path.join(BASE_DIR, "index.html");
     }
 
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
+    // Ensure the file exists before serving
+    fs.stat(filePath, (err, stats) => {
+        if (err || !stats.isFile()) {
             res.writeHead(404, { "Content-Type": "text/plain" });
             res.end("404 Not Found");
-        } else {
-            const ext = path.extname(filePath);
-            let contentType = "text/html";
-            if (ext === ".js") contentType = "application/javascript";
-            else if (ext === ".css") contentType = "text/css";
-
-            res.writeHead(200, { "Content-Type": contentType });
-            res.end(data);
+            return;
         }
+
+        // Determine content type
+        const ext = path.extname(filePath);
+        let contentType = "text/html";
+        if (ext === ".js") contentType = "application/javascript";
+        else if (ext === ".json") contentType = "application/json";
+        else if (ext === ".png") contentType = "image/png";
+
+        res.writeHead(200, { "Content-Type": contentType });
+        fs.createReadStream(filePath).pipe(res);
     });
 });
 
